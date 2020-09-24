8ed338b43d85546a5642b3a89bf3b340537c2962
Kharleann Cabrera <kharleann.cabrera@eyss.io>
2020-09-23 23:42:23 -0400

    Hooks and responsive

--- src/components/ThreeJS/FirstScene.js
+++ src/components/ThreeJS/FirstScene.js
@@ -1,61 +1,98 @@
-import React, { Component } from "react";
+import React from "react";
+import "./canvas.css";
 import * as THREE from "three";
-class ThreeScene extends Component {
-	componentDidMount() {
-		const width = this.mount.clientWidth;
-		const height = this.mount.clientHeight;
-		//ADD SCENE
-		this.scene = new THREE.Scene();
-		//ADD CAMERA
-		this.camera = new THREE.PerspectiveCamera(
+
+const FirstScene = () => {
+	const { useRef, useEffect, useState } = React;
+	const mount = useRef(null);
+	const [isAnimating, setAnimating] = useState(true);
+	const controls = useRef(null);
+
+	useEffect(() => {
+		let width = mount.current.clientWidth;
+		let height = mount.current.clientHeight;
+		let frameId;
+
+		const scene = new THREE.Scene();
+		const camera = new THREE.PerspectiveCamera(
 			75,
 			width / height,
 			0.1,
 			1000
 		);
-		this.camera.position.z = 4;
-		//ADD RENDERER
-		this.renderer = new THREE.WebGLRenderer({ antialias: true });
-		this.renderer.setClearColor("#000000");
-		this.renderer.setSize(width, height);
-		this.mount.appendChild(this.renderer.domElement);
-		//ADD CUBE
+		const renderer = new THREE.WebGLRenderer({ antialias: true });
 		const geometry = new THREE.BoxGeometry(1, 1, 1);
-		const material = new THREE.MeshBasicMaterial({ color: "#433F81" });
-		this.cube = new THREE.Mesh(geometry, material);
-		this.scene.add(this.cube);
-		this.start();
-	}
-	componentWillUnmount() {
-		this.stop();
-		this.mount.removeChild(this.renderer.domElement);
-	}
-	start = () => {
-		if (!this.frameId) {
-			this.frameId = requestAnimationFrame(this.animate);
+		const material = new THREE.MeshBasicMaterial({ color: 0xff00ff });
+		const cube = new THREE.Mesh(geometry, material);
+
+		camera.position.z = 4;
+		scene.add(cube);
+		renderer.setClearColor("#000000");
+		renderer.setSize(width, height);
+
+		const renderScene = () => {
+			renderer.render(scene, camera);
+		};
+
+		const handleResize = () => {
+			width = mount.current.clientWidth;
+			height = mount.current.clientHeight;
+			renderer.setSize(width, height);
+			camera.aspect = width / height;
+			camera.updateProjectionMatrix();
+			renderScene();
+		};
+
+		const animate = () => {
+			cube.rotation.x += 0.01;
+			cube.rotation.y += 0.01;
+
+			renderScene();
+			frameId = window.requestAnimationFrame(animate);
+		};
+
+		const start = () => {
+			if (!frameId) {
+				frameId = requestAnimationFrame(animate);
+			}
+		};
+
+		const stop = () => {
+			cancelAnimationFrame(frameId);
+			frameId = null;
+		};
+
+		mount.current.appendChild(renderer.domElement);
+		window.addEventListener("resize", handleResize);
+		start();
+
+		controls.current = { start, stop };
+
+		return () => {
+			stop();
+			window.removeEventListener("resize", handleResize);
+			mount.current.removeChild(renderer.domElement);
+
+			scene.remove(cube);
+			geometry.dispose();
+			material.dispose();
+		};
+	}, []);
+
+	useEffect(() => {
+		if (isAnimating) {
+			controls.current.start();
+		} else {
+			controls.current.stop();
 		}
-	};
-	stop = () => {
-		cancelAnimationFrame(this.frameId);
-	};
-	animate = () => {
-		this.cube.rotation.x += 0.01;
-		this.cube.rotation.y += 0.01;
-		this.renderScene();
-		this.frameId = window.requestAnimationFrame(this.animate);
-	};
-	renderScene = () => {
-		this.renderer.render(this.scene, this.camera);
-	};
-	render() {
-		return (
-			<div
-				style={{ width: "100vw", height: "100vh" }}
-				ref={(mount) => {
-					this.mount = mount;
-				}}
-			/>
-		);
-	}
-}
-export default ThreeScene;
+	}, [isAnimating]);
+
+	return (
+		<div
+			className="canvas"
+			ref={mount}
+			onClick={() => setAnimating(!isAnimating)}
+		/>
+	);
+};
+export default FirstScene;

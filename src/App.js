4d686fbbcb8646f23946677f9b1322f9a155817e
Kharleann Cabrera <kharleann.cabrera@eyss.io>
2020-09-23 23:06:29 -0400

    first push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import FirstScene from "./components/ThreeJS/FirstScene";
+
+function App() {
+	return (
+		<Fragment>
+			<FirstScene />
+		</Fragment>
+	);
+}
+
+export default App;

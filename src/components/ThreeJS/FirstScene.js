8fc10854718e58cc9a3b75b01c6de63db05f5de6
Kharleann Cabrera <kharleann.cabrera@eyss.io>
2020-09-25 00:40:50 -0400

    added camara and lights

--- src/components/ThreeJS/FirstScene.js
+++ src/components/ThreeJS/FirstScene.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./canvas.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { ImprovedNoise } from "three/examples/jsm/math/ImprovedNoise";
 
 const FirstScene = () => {
-	const { useRef, useEffect, useState } = React;
 	const mount = useRef(null);
 	const [isAnimating, setAnimating] = useState(true);
-	const controls = useRef(null);
+
+	const generateHeight = (width, height) => {
+		var size = width * height,
+			data = new Uint8Array(size),
+			perlin = new ImprovedNoise(),
+			quality = 1,
+			z = Math.random() * 100;
+
+		for (var j = 0; j < 4; j++) {
+			for (var i = 0; i < size; i++) {
+				var x = i % width,
+					y = ~~(i / width);
+				data[i] += Math.abs(
+					perlin.noise(x / quality, y / quality, z) * quality * 1.75
+				);
+			}
+
+			quality *= 5;
+		}
+
+		return data;
+	};
 
 	useEffect(() => {
 		let width = mount.current.clientWidth;
@@ -20,13 +42,48 @@ const FirstScene = () => {
 			0.1,
 			1000
 		);
+		const controls = new OrbitControls(camera, mount.current);
+		controls.update();
 		const renderer = new THREE.WebGLRenderer({ antialias: true });
-		const geometry = new THREE.BoxGeometry(1, 1, 1);
-		const material = new THREE.MeshBasicMaterial({ color: 0xff00ff });
-		const cube = new THREE.Mesh(geometry, material);
+		let geometry = new THREE.BoxGeometry(1, 1, 1);
+		let material = new THREE.MeshLambertMaterial({
+			color: 0xff00ff,
+			reflectivity: 0.5,
+		});
+		let light = new THREE.SpotLight(0xff00ff, 1);
+		light.position.set(0, 10, 40);
 
-		camera.position.z = 4;
+		const cube = new THREE.Mesh(geometry, material);
+		light.target = cube;
+		geometry = new THREE.PlaneBufferGeometry(
+			100,
+			100,
+			width - 1,
+			height - 1
+		);
+		material = new THREE.MeshLambertMaterial({
+			color: 0x00aaaa,
+			side: THREE.DoubleSide,
+			reflectivity: 0.1,
+		});
+
+		let vertices = geometry.attributes.position.array;
+		//const data = generateHeight(width, height);
+
+		//	for (let i = 0, j = 0, l = vertices.length; i < l; i++, j += 3) {
+		//		vertices[j + 1] = data[i] * 0.2;
+		//	}
+
+		const floor = new THREE.Mesh(geometry, material);
+
+		floor.position.y = -5;
+		floor.rotation.x = -Math.PI / 2;
+		camera.position.z = -10;
+		camera.position.y = 10;
+
+		scene.add(light);
 		scene.add(cube);
+		scene.add(floor);
 		renderer.setClearColor("#000000");
 		renderer.setSize(width, height);
 
@@ -44,9 +101,7 @@ const FirstScene = () => {
 		};
 
 		const animate = () => {
-			cube.rotation.x += 0.01;
-			cube.rotation.y += 0.01;
-
+			controls.update();
 			renderScene();
 			frameId = window.requestAnimationFrame(animate);
 		};
@@ -66,8 +121,6 @@ const FirstScene = () => {
 		window.addEventListener("resize", handleResize);
 		start();
 
-		controls.current = { start, stop };
-
 		return () => {
 			stop();
 			window.removeEventListener("resize", handleResize);
@@ -79,20 +132,6 @@ const FirstScene = () => {
 		};
 	}, []);
 
-	useEffect(() => {
-		if (isAnimating) {
-			controls.current.start();
-		} else {
-			controls.current.stop();
-		}
-	}, [isAnimating]);
-
-	return (
-		<div
-			className="canvas"
-			ref={mount}
-			onClick={() => setAnimating(!isAnimating)}
-		/>
-	);
+	return <div className="canvas" ref={mount} />;
 };
 export default FirstScene;
